25092a01fa92e24f876948675b01745446e5878f
farzamkhan <farzam@live.com>
2019-07-17 13:40:36 +0430

    tamrin2 input

---
+++ tamrin2.py
@@ -0,0 +1,13 @@
+num1 = int(input("Please Enter Num1: "))
+num2 = int(input("Please Enter Num2: "))
+
+
+tavan = num1 ** num2
+print ("Num1 ** Num2 = ", tavan)
+
+baghimande = num2 % num1
+print ("baghimande Num2 bar Num1 =", baghimande)
+
+
+shart = num1 != num2
+print ("Num1 != Num2 ?", shart)
