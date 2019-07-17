299e5d8b580b8ed0a3fbb25e379ddefda5c73319
farzamkhan <farzam@live.com>
2019-07-17 11:56:14 +0430

    jam zarb tavan mosavi namosavi

---
+++ tamrin1-1.py
@@ -0,0 +1,30 @@
+a = 8
+b = 3
+c = 4
+
+print ("A =", a)
+print ("B =", b)
+print ("C =", c)
+
+jam = a + b
+print ("jam A,B =", jam)
+
+
+zarb = b * c
+print ("zarb B,C =", zarb)
+
+tafrigh = a - c
+print ("tafrigh A,C =", tafrigh)
+
+
+taghsim = a / b 
+print ("taghsim A,B =", taghsim)
+
+
+b_taghsim = a % b
+print("baghimandeye taghsim A,B =", b_taghsim)
+
+taghsim_s = a // b
+print ("taghsim sahih A,B =", taghsim_s)
+
+
---
+++ tamrin1-2.py
@@ -0,0 +1,17 @@
+a = 8
+b = 3
+c = 4
+d = 3
+
+print ("A =", a)
+print ("B =", b)
+print ("C =", c)
+print ("D =", d)
+
+print ("A > B ?", a > b)
+print ("C < B ?", c < b)
+print ("B <= D ?", b <=d)
+print ("A**b =", a ** b)
+print ("A != B", a != b)
+print ("B != D", b != d)
+
